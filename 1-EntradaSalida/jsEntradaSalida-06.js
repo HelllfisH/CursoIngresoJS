62972498ffd07c31c107e9b2510af17b2b866e6d
HelllfisH <rodriguezmarcoscruz@gmail.com>
2022-02-08 01:26:39 -0300

    codigo_ejercicio JS_01

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -6,14 +6,15 @@ function sumar()
 {
 	var number1;
 	var number2;
-	var suma_total;
+	var resultado;
 	number1= txtIdNumeroUno.value;
 	number2= txtIdNumeroDos.value;
 	number1= parseInt(number1);
 	number2= parseInt(number2);
-	suma_total= number1 + number2;
-	alert("La suma es: " + suma_total);
+	resultado= number1 + number2;
+	alert("La suma es " + resultado);
 	/*el parseInt() comprueba el primer argumento, una cadena y lo
 	y lo tranforma en un numero entero*/
+
 }
 
