6cf2a52fbd28bbfcb9bb5c5a59c88642e4d5b8e5
HelllfisH <rodriguezmarcoscruz@gmail.com>
2022-02-18 14:51:52 -0300

    codigo_(switch)instruccion JS_02

--- 3-InstruccionSwitch/jsInstruccionSwitch-09.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-09.js
@@ -1,20 +1,115 @@
 function mostrar()
 {
-	var viajes;
+	var destino;
 	var estadia;
+	var estaciones;
+	var aumento;
+	var descuento;
+	estaciones= txtIdEstacion.value;
+	destino= txtIdDestino.value;
 	estadia= 15000;
 
-	switch(estadia)
+	switch(estaciones)
 	{
 		case "Invierno":
-			break;
+			switch(destino)
+			{
+				case "Bariloche":
+					aumento= estadia * 0.20;
+					destino= estadia + aumento;
+					alert("El valor es de " + destino);
+					break;
+				case "Cataratas":
+					descuento= estadia * 0.10;
+					destino= estadia - descuento;
+					alert("El valor es de " + destino);
+					break;
+				case "Cordoba":
+					descuento= estadia * 0.10;
+					destino= estadia - descuento;
+					alert("El valor es de " + destino);
+					break;
+				case "Mar del plata":
+					descuento= estadia * 0.20;
+					destino= estadia - descuento;
+					alert("El valor es de " + destino);
+					break;			
+			}
+			break;				
 		case "Verano":
+			switch(destino)
+			{
+				case "Bariloche":
+					descuento= estadia * 0.20;
+					destino= estadia - descuento;
+					alert("El valor es de " + destino);
+					break;
+				case "Cataratas":
+					aumento= estadia * 0.10;
+					destino= estadia + aumento;
+					alert("El valor es de " + destino);
+					break;
+				case "Cordoba": 
+					aumento= estadia * 0.10;
+					destino= estadia + aumento;
+					alert("El valor es de " + destino);
+					break;
+				case "Mar del plata":
+					aumento= estadia * 0.20;
+					destino= estadia + aumento;
+					alert("El valor es de " + destino);
+					break;			
+			}
 			break;
 		case "Otoño":
+			switch(destino)
+			{
+				case "Bariloche":
+					aumento= estadia * 0.10;
+					destino= estadia + aumento;
+					alert("El valor es de " + destino);
+					break;
+				case "Cataratas":
+					aumento= estadia * 0.10;
+					destino= estadia + aumento;
+					alert("El valor es de " + destino);
+					break;
+				case "Mar del plata":
+					aumento= estadia * 0.10;
+					destino= estadia + aumento;
+					alert("El valor es de " + destino);
+					break;
+				case "Cordoba":
+					alert("El valor es de " + estadia);
+					break;			
+			}
 			break;
+
 		case "Primavera":
+			switch(destino)
+			{
+				case "Bariloche":
+					aumento= estadia * 0.10;
+					destino= estadia + aumento;
+					alert("El valor es de " + destino);
+					break;
+				case "Cataratas":
+					aumento= estadia * 0.10;
+					destino= estadia + aumento;
+					alert("El valor es de " + destino);
+					break;
+				case "Mar del plata":
+					aumento= estadia * 0.10;
+					destino= estadia + aumento;
+					alert("El valor es de " + destino);
+					break;
+				case "Cordoba":
+					alert("El valor es de " + estadia);
+					break;
+			}
 			break;
-						
+		default:
+			break;				
 
 	}
 
--- 3-InstruccionSwitch/jsInstruccionSwitch-10.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-10.js
@@ -1,6 +1,66 @@
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	var estaciones;
+	var destino;
+	estaciones= txtIdEstacion.value;
+	destino= txtIdDestino.value;
+
+	switch(estaciones)
+	{
+		case "Invierno":
+			switch(destino)
+			{
+				case "Bariloche":
+					alert("Se viaja");
+					break;
+				case "Cordoba":
+				case "Cataratas":
+				case "Mar del plata":
+					alert("No se viaja");
+					break;
+			}
+			break;
+		case "Verano":
+			switch(destino)
+			{
+				case "Cataratas":
+				case "Mar del plata":
+					alert("Se viaja");
+					break;
+				case "Bariloche":
+				case "Cordoba":
+					alert("No se viaja");
+					break;
+			}	
+			break;
+		case "Otoño":
+			switch(destino)
+			{
+				case "Bariloche":
+				case "Cordoba":
+				case "Cataratas":
+				case "Mar del plata":
+					alert("Se viaja");
+					break;		
+			}	
+			break;
+		case "Primavera":
+			switch(destino)
+			{
+				case "Bariloche":
+					alert("No se viaja");
+					break;
+				case "Cordoba":
+				case "Cataratas":
+				case "Mar del plata":
+					alert("Se viaja");
+					break;			
+			}	
+			break;
+		default:
+			break;	
+	}		
+
+
 
 }//FIN DE LA FUNCIÓN
