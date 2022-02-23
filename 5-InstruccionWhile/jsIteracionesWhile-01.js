e8ea1a18f0eb2b5cdc11feb75e22faa44ec740f4
HelllfisH <rodriguezmarcoscruz@gmail.com>
2022-02-23 14:54:24 -0300

    codigo_(while)instruccion JS_02

--- 5-InstruccionWhile/jsIteracionesWhile-01.js
+++ 5-InstruccionWhile/jsIteracionesWhile-01.js
@@ -3,5 +3,14 @@ al presionar el botón mostrar 10 repeticiones
 con números ASCENDENTE, desde el 1 al 10.*/
 function mostrar()
 {
-	alert('iteración while');
+	var contador = 0;
+
+	do
+	{
+		contador++;
+		alert("contador = " + contador);
+	
+	}while(contador < 10)
+	
+	//alert('iteración while');
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteracionesWhile-03.js
+++ 5-InstruccionWhile/jsIteracionesWhile-03.js
@@ -2,7 +2,22 @@
 al presionar el botón pedir la CLAVE (ayuda: es utn750)*/
 function mostrar()
 {
-	var claveIngresada;
-	claveIngresada = prompt("ingrese el número clave.");
+	var clave;
+	clave= prompt("Ingrese la clave");
+	
+	do
+	{
+		if(clave != "utn750")
+		{
+			alert("La clave es incorrecta");
+			clave= prompt("Vuelve a ingresar la clave");
+
+		}else
+		{
+			alert("Clave correcta");
+		}
+		
+	}while(clave != "utn750")
+		
 	
 }//FIN DE LA FUNCIÓN
